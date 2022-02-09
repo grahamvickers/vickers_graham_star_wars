e4113c288e4947c9cc674581f6c668cff690b35c
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-02-09 02:55:58 -0500

    fil display added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 // created a component to show the data on the front-end
 Vue.component('character-list', {
+    // this is the template that will display on the front end html
     template: 
     `<form>
         <div id="searchCon">
@@ -9,7 +10,7 @@ Vue.component('character-list', {
         </div>
         <ul id="characterList">
             <li v-for="character in filteredCharacters">
-                <a v-on:click="">{{character.name}}</a>
+                <a onclick="mockup()" v-on:click="">{{character.name}}</a>
             </li>
         </ul>
     </form>`,
@@ -19,7 +20,16 @@ Vue.component('character-list', {
             searchResult: ""
         }
     },
+
+    // methods: {
+    //     clicked: function(event) {
+    //         console.log(event.currentTarget.id);
+    //         // characterId = e.currentTarget.id;
+    //     }
+    // },
+
     props: ['thecharacters'],
+
     computed: {
         filteredCharacters: function() {
             let characters_array = this.thecharacters;
@@ -31,6 +41,7 @@ Vue.component('character-list', {
 
             searchResult = searchResult.trim().toLowerCase();
 
+            // searching array and displaying results
             characters_array = characters_array.filter(function(droid) {
                 //console.log(droid.name.toLowerCase().indexOf(searchResult));
                 if(droid.name.toLowerCase().indexOf(searchResult) !== -1) {
@@ -42,12 +53,35 @@ Vue.component('character-list', {
         }
     }
 });
+
+// create the clicked character component to display the film
+// 
+// Vue.component('character-clicked', {
+//     template:
+//     `<div>
+//         <img v-bind:src="filmName">
+//         <h3>{{films.title}}<h3>
+//         <p>{{films.opening_crawl}}<p>
+//     </div>`,
+
+//     props: ['filmName', 'thefilms'],
+//     computed: {
+//         filmOptions: function() {
+//             let film_array = this.film;
+//             return film_array;
+//         }
+//     }
+
+// });
+
 // create a new vue model
 let vm = new Vue({
     el: "#app",
     data: {
-        characters: []
+        characters: [],
+        films: []
     },
+    // fetch all the characters from the api
     created: function() {
         fetch('https://swapi.dev/api/people/')
         .then(function(response){
@@ -57,6 +91,29 @@ let vm = new Vue({
             //console.log(data)
             vm.characters = data.results;
         })
-    }
-
+    },
+    // clicked: function() {
+    //     fetch('https://swapi.dev/api/films/')
+    //     .then(function(response){
+    //         return response.json();
+    //     })
+    //     .then(function(filmData){
+    //         //console.log(data)
+    //         vm.films = filmData.results;
+    //     })
+    // }    
 }); 
+
+function lightsaber(){
+    var soundEffect = new Audio('./audio/lightsaber.mp3');
+    soundEffect.play();
+}
+
+function mockup() {
+    var filmMockup = document.getElementById("filmCon");
+    if (filmMockup.style.display === "none") {
+        filmMockup.style.display = "flex";
+    } else {
+        filmMockup.style.display = "none";
+    }
+  }
