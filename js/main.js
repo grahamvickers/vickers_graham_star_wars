8cb7a7a99bb7806e516b2f12ec5d1ff72b3afdb3
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-02-09 00:15:25 -0500

    api and styles done

--- js/main.js
+++ js/main.js
@@ -4,8 +4,43 @@
 Vue.component('character-list', {
     template: 
     `<form>
-        
-    </form>`
+        <div id="searchCon">
+            <input type="text" placeholder="ex. R2D2" v-model="searchResult" id="search"/>
+        </div>
+        <ul id="characterList">
+            <li v-for="character in filteredCharacters">
+                <a v-on:click="">{{character.name}}</a>
+            </li>
+        </ul>
+    </form>`,
+
+    data: function() {
+        return {
+            searchResult: ""
+        }
+    },
+    props: ['thecharacters'],
+    computed: {
+        filteredCharacters: function() {
+            let characters_array = this.thecharacters;
+            let searchResult = this.searchResult;
+
+            if(searchResult==="") {
+                return characters_array;
+            }
+
+            searchResult = searchResult.trim().toLowerCase();
+
+            characters_array = characters_array.filter(function(droid) {
+                //console.log(droid.name.toLowerCase().indexOf(searchResult));
+                if(droid.name.toLowerCase().indexOf(searchResult) !== -1) {
+                    return droid;
+                }
+            })
+
+            return characters_array;
+        }
+    }
 });
 // create a new vue model
 let vm = new Vue({
@@ -19,8 +54,9 @@ let vm = new Vue({
             return response.json();
         })
         .then(function(data){
-            console.log(data)
+            //console.log(data)
             vm.characters = data.results;
         })
     }
+
 }); 
